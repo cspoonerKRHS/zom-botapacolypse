7af68417a2c30586ed8082e828648224f398bc0b
Matthew Hahn <matthew.hahn@kearsarge.org>
2013-02-18 09:00:43 -0500

    more edits to stick

--- Stick.py
+++ Stick.py
@@ -22,6 +22,7 @@ class Stick():
         self.rect = self.surface.get_rect()
         self.place(position)
         self.notBroken = True
+        self.hitCount = 10
         #if pygame.mixer:
         #    self.stickSound = pygame.mixer.Sound("stick.wav")
            
@@ -31,22 +32,39 @@ class Stick():
     def place(self, position):
         self.rect.center = position
         #print "I've moved to", position
-        
-    def attack(self, other):
-        print "I've attacked", str(other)
-        
-    def collideZombie(self, other):
+      
+    def collideZombie(self, zombie):
+        if (self.rect.right > zombie.rect.left 
+            and self.rect.left < zombie.rect.right):
+            if (self.rect.bottom > zombie.rect.top and 
+                self.rect.top < zombie.rect.bottom):
+                if (self.distToPoint(zombie.rect.center)
+                    < self.radius + zombie.radius):  
+                    self.hitCount = self.hitCount -1
+                    zombie.life = zombie.life-10
         print "I've hit the zombie", str(other)
         
-    def collideRobot(self, other):
+    def collideRobot(self, robot):
+        if (self.rect.right > robot.rect.left 
+                and self.rect.left < robot.rect.right):
+                if (self.rect.bottom > robot.rect.top and 
+                    self.rect.top < robot.rect.bottom):
+                    if (self.distToPoint(robot.rect.center)
+                        < self.radius + robot.radius):  
+                        self.hitCount = self.hitCount -1
+                        robot.life = robot.life-10
         print "I've hit the Robot", str(other)
         
-    def collideMazeWall(self, other):
+    def collideMazeWall(self, Mazewall):
+        if (self.rect.right > MazeWall.rect.left and
+                self.rect.left < MazeWall.rect.right):
+                    if (self.rect.bottom > MazeWall.rect.top and 
+                        self.rect.top < MazeWall.rect.bottom):
+                            if (self.distToPoint(MazeWall.rect.center) < 
+                                self.radius + MazeWall.radius): 
+                                    self.notBroken = False
         print "I've hit the MazeWall", str(other)
-        
-    def useDown(self):
-        print "I've been used"
-    
+            
     def broken(self):
         print "I'm broken"
         
